42b08908b309e1e423cf2235884381a875304554
henry <asdcxz179@gmail.com>
2018-06-30 18:54:18 +0800

    kkk

---
+++ game.php
@@ -0,0 +1,162 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<title>WebSocket</title>
+<script type="text/javascript" src="jquery.min.js"></script>
+<script type="text/javascript">
+var name;
+if(name==''){
+	name = prompt("输入名称","");
+}
+
+var ws = new WebSocket("ws://192.168.117.111:1024");
+var data = {};
+ws.onopen = function(){
+    console.log("握手成功");
+    into();
+};
+ws.onmessage = function(e){
+    console.log("message:" + e.data);
+    data = JSON.parse(e.data);
+    console.log(data);
+    switch (data.action){
+    	case 'list':
+    		var str ='';
+    		$.each(data.data,function(i,e){
+    			str +='<p>'+e+'</p>';
+    		});
+    		$('.left_nav').html(str);
+    		var str ='';
+    		var check = false;
+    		$.each(data.room,function(i,e){
+    			if(!check && e.host == name){
+    				check = true;
+    			}
+    			str += '<div class="room"><p>number:'+i+'</p>';
+    			str += '<p>name:'+e.name+'</p>';
+    			str += '<p>total:'+e.total+'</p>';
+    			str += '<p>limit:'+e.limit+'</p>';
+
+    			if(Object.values(e.people).indexOf(name)>-1 ){
+    				$('#create').remove();
+    				if(e.host == name){
+    					str += '<button id="start" data-num="'+i+'">開始遊戲</button>';	
+    				}
+    				str += '<button id="leave_room" data-num="'+i+'">離開房間</button>';
+    			}else{
+    				str += '<button id="join" data-num="'+i+'">加入房間</button>';
+    			}
+    			str += '</div>';
+    		});
+    		$('.hall').html(str);
+    		var str = '<button id="create">建立房間</button>';
+    		if(!check){
+    			$('.fun').html(str);
+    			
+    		}else{
+    			$('#join').remove();
+    		}
+    	break;
+    	case 'close':
+    		alert('名稱重複,請重新命名');
+    		name = '';
+   			window.location.reload();
+    	break;
+    }
+};
+ws.onclose =function(){
+	// alert("伺服器連線中斷");
+	console.log("伺服器連線中斷");
+}
+ws.onerror = function(){
+    console.log("error");
+};
+function into(){
+	data.action = 'into';
+	data.id = name;
+	ws.send(JSON.stringify(data));
+}
+$(document).ready(function(){
+	
+	// $('#start').click(function(){
+	// 	id = Math.floor(Math.random()*10000);
+	// 	room = true;
+	// 	player = 'master';
+	// 	players++;
+	// 	game = '{"player":"'+player+'","action":"create"}';
+	// 	$('#room_number').text("房間代碼:"+id);
+	// 	ready = 1;
+	// 	ws.send(game);
+	// });
+});
+$(document).on('click','#create',function(){
+	$(this).remove();
+	data.action = 'create';
+	ws.send(JSON.stringify(data));
+})
+$(document).on('click','#join',function(){
+	$(this).remove();
+	data.action = 'join';
+	data.num = $(this).data('num');
+	ws.send(JSON.stringify(data));
+})
+$(document).on('click','#leave_room',function(){
+	$(this).remove();
+	data.action = 'leave_room';
+	data.num = $(this).data('num');
+	ws.send(JSON.stringify(data));
+})
+
+</script>
+<style type="text/css">
+	.left_nav{
+		width: 200px;
+	    text-align: center;
+	    float: left;
+	    border: 1px solid;
+	    height: 95%;
+	    overflow-y: auto;
+	}
+	.hall{
+		float: left;
+	}
+	.room{
+		border: 1px solid;
+	    text-align: center;
+	    width: 200px;
+	    margin: 0 15px 10px 15px;
+	}
+</style>
+</head>
+
+<body>
+ 	
+ 	<div class="nav">
+ 		大廳列表
+ 		<div class="fun">
+ 			
+ 		</div>
+ 	</div>
+ 	<div class="left_nav"></div>
+ 	<div class="hall">
+ 		
+ 	</div>
+ 	<!-- <div id="room_number"></div>
+ 	<div id="start">建立遊戲</div>
+ 	<div id="join">加入遊戲</div>
+ 	<input type="text" name="join" id="room_name">
+ 	<div id="set_game" style="display:none;">
+	 	<div id="sendnumber">送出你的數字</div>
+	 	<input type="text" name="number" id="number">
+	</div>
+	<div id="play_game" style="display:none;">
+		<div id="choice">送出你猜數字</div>
+	 	<input type="text" name="choice" id="choicenumber">
+	 	<div id="master"><h3>master</h3></div>
+	 	<div id="custom"><h3>custom</h3></div>
+	</div> -->
+</body>
+<script type="text/javascript">
+	// $('.nav').html(name);
+</script>
+</html>
---
+++ server5.php
@@ -0,0 +1,266 @@
+<?php
+	
+	include('tools.php');
+	class Socket extends tools{
+
+
+		public $hall=[];
+		public $room=[];
+
+		public function __construct($ip,$port){
+			parent::__construct();
+			$this->message = true;
+			$this->ip = $ip;
+			$this->port = $port;
+			$this->log = "game_logs.txt";
+		}
+
+		public function run(){
+			$this->master=socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+			socket_set_option($this->master, SOL_SOCKET, SO_REUSEADDR, 1);
+			socket_bind($this->master, $this->ip,$this->port);
+			socket_listen($this->master);	
+
+			$this->clients = [];
+			$this->_logs($this->log,"啟動socket , ip:".$this->ip." ,port:".$this->port);
+			$this->_logs($this->log,"主連接線  : ".$this->master);
+			$this->_logs($this->log,"-------------------");
+			$handshake=false;
+			while (true) {
+				$write = NULL;
+		        $except = NULL;
+		        $this->sockets = $this->clients;
+		        $this->sockets[] = $this->master;
+		        // print_r($this->sockets);
+		        socket_select($this->sockets, $write, $except, NULL);
+
+		        $i = 1; 
+		        foreach ($this->sockets as $socket) {
+		    		if($socket==$this->master)
+		    		{
+		    			$this->accept();
+		    		}else{
+		    			$this->client = $socket;
+		    			$this->write($socket);
+		    		}
+				}
+				$this->_logs($this->log,"-------------------");
+			}
+		} 
+
+		public function accept(){
+			$this->client=socket_accept($this->master);
+			$header = socket_read($this->client, 1024);
+			$this->sockets[] = $this->client;
+			$this->clients[] = $this->client;
+			if (preg_match("/Sec-WebSocket-Key: (.*)\r\n/", $header, $match))//冒号后面有个空格
+		    {
+		        $secKey = $match[1];
+		        $secAccept = base64_encode(sha1($secKey . '258EAFA5-E914-47DA-95CA-C5AB0DC85B11', TRUE));//握手算法固定的
+		        $upgrade  = "HTTP/1.1 101 Web Socket Protocol Handshake\r\n" .
+		        "Upgrade: websocket\r\n" .
+		        "Connection: Upgrade\r\n" .
+		        "Sec-WebSocket-Accept:$secAccept\r\n\r\n";
+		        socket_write($this->client, $upgrade, strlen($upgrade));
+		    }
+		}
+
+		public function process_data($data){
+			$data = json_decode($data,true);
+			$this->_logs($this->log,"接收資料".json_encode($data));
+			switch ($data['action']) {
+				case 'into':
+					if(in_array($data['id'],$this->hall)){
+						$this->_logs($this->log,"使用者:".$data['id']."重複踢出該使用者");
+						return $this->kick_out($this->client,json_encode(['action'=>'close','msg'=>'']));
+					}else{
+						$check = true;
+						foreach ($this->room as $k => $room) {
+							if(in_array($data['id'], $room['people'])){
+								$check = false;
+								$room_number = $k;
+							}
+						}
+						if($check){
+							$this->hall[$this->search_index($this->client)] = $data['id'];	
+							$this->_logs($this->log,"使用者:".$data['id']."進入大廳");
+							$this->_logs($this->log,"大廳人員:".json_encode($this->hall));
+						}else{
+							$this->_logs($this->log,"使用者:".$data['id']."已經在房間,編號".$room_number);
+						}
+						
+						$return['action'] = 'list';
+						$return['data'] = $this->hall;
+						$return['room'] = $this->room;
+						
+					}
+					break;
+				case 'create':
+					$room_number = substr(md5(uniqid(rand(), true)),0,10);
+					$limit = 6;
+					$index = $this->search_index($this->client);
+					$this->_logs($this->log,"index:".$index);
+					$user = $this->hall[$index];
+					$this->_logs($this->log,$this->hall[$index]);
+					unset($this->hall[$index]);
+					$this->room[$room_number]['name'] = "五子棋";
+					$this->room[$room_number]['host'] = $user;
+					$this->room[$room_number]['limit'] = $limit;
+					$this->room[$room_number]['people'][$index] = $user;
+					$this->room[$room_number]['total'] = count($this->room[$room_number]['people']);
+					$this->_logs($this->log,"使用者:".$user."建立房間,編號:".$room_number);
+					$return['action'] = 'list';
+					$return['data'] = $this->hall;
+					$return['room'] = $this->room;
+				break;
+				case 'join':
+					$room_number = $data['num'];
+					$index = $this->search_index($this->client);
+					if(count($this->room[$room_number]['people'])<$this->room[$room_number]['limit']){
+						$user = $this->hall[$this->search_index($this->client)];
+						unset($this->hall[$this->search_index($this->client)]);
+						$this->room[$room_number]['people'][$index] = $user;
+						$this->room[$room_number]['total'] = count($this->room[$room_number]['people']);
+						$this->_logs($this->log,"使用者:".$user."加入房間成功,編號:".$room_number);
+					}else{
+						$this->_logs($this->log,"使用者:".$user."加入房間失敗,編號:".$room_number.",人數已滿");
+					}
+					$return['action'] = 'list';
+					$return['data'] = $this->hall;
+					$return['room'] = $this->room;
+				break;
+				case 'leave_room':
+					$room_number = $data['num'];
+					$index = $this->search_index($this->client);
+					$this->leave_room($index);
+					$return['action'] = 'list';
+					$return['data'] = $this->hall;
+					$return['room'] = $this->room;
+				break;
+				case 'leave':
+					$return['action'] = 'list';
+					$return['data'] = $this->hall;
+					$return['room'] = $this->room;
+				break;
+				default:
+					$return = [];
+					break;
+			}
+			return json_encode($return);
+		}
+
+		public function search_index($socket){
+			return array_search($socket, $this->clients);
+		}
+
+		public function kick_out($socket,$text){
+			
+			$this->send_data($socket,$text);
+			$index = $this->search_index($socket);
+			$this->_logs($this->log,"連線號:".$index);
+			unset($this->clients[$index]);
+			socket_close($socket);
+			return false;
+		}
+
+		public function leave_hall($socket){
+			
+			$index = $this->search_index($socket);
+			$this->_logs($this->log,"連線號:".$index);
+			unset($this->clients[$index]);
+			$this->leave_room($index);
+			if(isset($this->hall[$index])){
+				$this->_logs($this->log,"使用者".$this->hall[$index]."離開大廳");	
+				unset($this->hall[$index]);
+			}
+			
+			$text = $this->process_data('{"action":"leave"}');
+			$this->send_data($this->clients,$text);
+			socket_close($socket);
+		}
+
+		public function leave_room($index){
+			foreach ($this->room as $k => $room) {
+				if(isset($room['people'][$index])){
+					$this->_logs($this->log,"使用者".$room['people'][$index]."離開房間");	
+					$this->hall[$index] = $this->room[$k]['people'][$index];
+					unset($this->room[$k]['people'][$index]);
+					$this->room[$k]['total'] = count($this->room[$k]['people']);
+				}
+				if(count($this->room[$k]['people'])==0){
+					unset($this->room[$k]);
+					$this->_logs($this->log,"房間".$k."無人關閉");	
+				}
+			}
+		}
+
+		public function write($socket){
+			$bytes = @socket_recv($socket, $buffer, 1024, 0);
+			$this->_logs($this->log,"資料長度:".$bytes);
+			if($bytes<=8){
+				$this->leave_hall($socket);
+			}else{
+    			$length = ord($buffer[1]) & 127;
+			    if($length == 126) 
+			    {
+			        $masks = substr($buffer, 4, 4);
+			        $data = substr($buffer, 8);
+			    }
+			    elseif($length == 127) 
+			    {
+			        $masks = substr($buffer, 10, 4);
+			        $data = substr($buffer, 14);
+			    }
+			    else
+			    {
+			        $masks = substr($buffer, 2, 4);
+			        $data = substr($buffer, 6);
+			    }
+			    $text = "";
+			    for ($i = 0; $i < strlen($data); ++$i) 
+			    {
+			        $text .= $data[$i] ^ $masks[$i%4];
+			    }
+			    
+			    $text = $this->process_data($text);
+			    if($text){
+			    	$this->send_data($this->clients,$text);	
+			    }
+			    
+		    }
+		}
+
+		public function send_data($target,$text){
+			$this->_logs($this->log,"傳送資料:".$text);
+			$b1 = 0x80 | (0x1 & 0x0f);
+		    $length = strlen($text);
+		 
+		    if($length <= 125)
+		    {
+		        $header = pack('CC', $b1, $length);
+		    }
+		    elseif($length > 125 && $length < 65536)
+		    {
+		        $header = pack('CCn', $b1, 126, $length);
+		    }
+		    elseif($length >= 65536)
+		    {
+		        $header = pack('CCNN', $b1, 127,0, $length);
+		    }
+
+			if(is_array($target)){
+				foreach ($target as $a) {
+			    	socket_write($a, $header.$text);
+			    }	
+			}else{
+				socket_write($target, $header.$text);
+			}
+		}
+
+	}
+
+	$socket = new Socket('192.168.117.111','1024');
+	$socket->run();
+
+
+?>
